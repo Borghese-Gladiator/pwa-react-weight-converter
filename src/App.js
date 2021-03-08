389bbfa8f18808d8942441510d5b16964be0e789
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-08 10:07:00 -0500

    create weight converted tutorial app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState({
+    'lbs': null,
+    'grams': null,
+    'kilograms': null,
+    'ounces': null
+  });
+  const [isVisible, setIsVisible] = useState(false);
+  const handleChange = (e) => {
+    let lbs = e.target.value;
+    let grams = lbs / 0.0022046;
+    let kiloGrams = lbs / 2.2046;
+    let ounces = lbs * 16;
+
+    setWeight({
+      'lbs': lbs,
+      'grams': grams,
+      'kilograms': kiloGrams,
+      'ounces': ounces
+    })
+    if (!isVisible) {
+      setIsVisible(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="display-4 text-center mb-3">
+            Weight Converter
+          </h1>
+          <div className="form-group">
+            <input value={weight['lbs']} onChange={handleChange} type="number" className="form-control form-control-lg" id="lbsInput" placeholder="Enter pounds.." required />
+          </div>
+          {
+            !isVisible
+              ? <></>
+              : (
+                <div id="output">
+                  <div className="card bg-primary mb-2">
+                    <div className="card-block">
+                      <h4>Grams: {weight['grams']}</h4>
+                    </div>
+                  </div>
+
+                  <div className="card bg-success mb-2">
+                    <div className="card-block">
+                      <h4>Kilograms: {weight['kilograms']}</h4>
+                    </div>
+                  </div>
+
+                  <div className="card bg-danger mb-2">
+                    <div className="card-block">
+                      <h4>Ounces: {weight['ounces']}</h4>
+                    </div>
+                  </div>
+                </div>
+              )}
+        </div>
+      </div>
     </div>
   );
 }
